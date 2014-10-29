2a5d6e8f7e154f23e79e9b948ec1df4c85b25694
Jason Hugh Sam <jhughsam@gmail.com>
2014-10-29 18:55:10 -0400

    updated database and app settings

--- cdj_proj/cdj_proj/settings.py
+++ cdj_proj/cdj_proj/settings.py
@@ -36,6 +36,7 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'clouddj',
 )
 
 MIDDLEWARE_CLASSES = (
@@ -58,8 +59,12 @@ WSGI_APPLICATION = 'cdj_proj.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'grumblr',
+        'USER': 'webapps',
+        'PASSWORD': 'fun',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
