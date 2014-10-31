db545af992948fe47c3c8fd118f0e498badc345f
aalique <aalique.grahame@gmail.com>
2014-10-30 22:44:53 -0400

    fixed models.py

--- cdj_proj/clouddj/models.py
+++ cdj_proj/clouddj/models.py
@@ -3,23 +3,6 @@ from django.contrib.auth.models import User
 
 
 # Create your models here.
-#class Post(Comment):
- #   photo = models.ImageField(upload_to="album-art", default='album-art/randompic.jpg')
-  #  song = models.FileField(upload_to="music")
-   # plays = models.IntegerField(default = 0)
-   # genre = models.CharField(max_length = 255)
-  #  
-   # def __unicode__(self):
-    #    return self.profile +": "+self.text
-
-
-#class Rating()
-
-
-# Post-music file,album art,rating (as foreign key in Rate),comments(as foreign key in Comment),number of plays,genre
-
-
-
 class Profile(models.Model):
     user = models.OneToOneField(User)
     followers = models.ManyToManyField('self', symmetrical=False, related_name="following")
@@ -27,18 +10,18 @@ class Profile(models.Model):
     def __unicode__(self):
          return self.user.username
 
-#class Hashtag(models.Model): 
- #   text = models.CharField()
+class Hashtag(models.Model): 
+    text = models.CharField(max_length=200)
 
 class Post(models.Model):
     profile = models.ForeignKey(Profile)
-    text = models.CharField(max_length = 255)
-  #  date = models.DateTimeField(auto_add_now=True)
+    text = models.CharField(max_length=255)
+    date = models.DateTimeField(auto_now_add=True)
     photo = models.ImageField(upload_to='album-art', default='album-art/randompic.jpg')
     song = models.FileField(upload_to="music")
-    plays = models.IntegerField(default = 0)
-    genre = models.CharField(max_length = 255)
-#    hashtags = models.ManyToManyField(Hashtag, related_name='posts')
+    plays = models.IntegerField(default=0)
+    genre = models.CharField(max_length=255)
+    hashtags = models.ManyToManyField(Hashtag, related_name='posts')
     
     def __unicode__(self):
         return self.profile.user.username +": "+self.text
@@ -48,17 +31,16 @@ class Comment(models.Model):
     profile = models.ForeignKey(Profile)
     post = models.ForeignKey(Post)
     text = models.CharField(max_length=200)
- #   date = models.DateTimeField(auto_add_now=true)
+    date = models.DateTimeField(auto_now_add=True)
     
     def __unicode__(self):
         return self.text
 
-#Rate -user(foreign key),#stars/discs/thumbs ups/clouds
 
 class Rating(models.Model):
     profile= models.ForeignKey(Profile)
     post = models.ForeignKey(Post)
-   # rating = models.IntegerField(min_value=1, max_value=5)
+    rating = models.IntegerField()
 
 
 
