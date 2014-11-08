8238bc3f4b6a918eb6f46727bab8cf4a99d1d6ab
Rahshel Brown <rahshelb@gmail.com>
2014-11-08 14:58:32 -0500

    added method to upload music

--- cdj_proj/clouddj/music_views.py
+++ cdj_proj/clouddj/music_views.py
@@ -1,3 +1,16 @@
 # Music editing-related actions
+from django.shortcuts import render
+
+from clouddj.models import *
+from clouddj.forms import *
 
 
+def upload(request):
+    if request.method == 'GET':
+        form = UploadMusicForm()
+    else:
+        form = UploadMusicForm(request.POST, request.FILES)
+        if form.is_valid():
+            return render(request, 'edit.html', {})
+
+    render(request, 'upload.html', {'form': form})
--- cdj_proj/clouddj/urls.py
+++ cdj_proj/clouddj/urls.py
@@ -1,5 +1,10 @@
 from django.conf.urls import url
 
 urlpatterns = [
+    #social urls
     url(r'^$', 'clouddj.social_views.home', name='home'),
+
+    #music urls
+    url(r'^upload$', 'clouddj.music_views.upload', name='upload')
+
 ]
