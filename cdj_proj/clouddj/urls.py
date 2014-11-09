8823ce91ab2cbb5cfc6744561de9193bde9463b3
Rahshel Brown <rahshelb@gmail.com>
2014-11-09 10:47:23 -0500

    updated templates to use static refs and url tags; updated urls file

--- cdj_proj/clouddj/urls.py
+++ cdj_proj/clouddj/urls.py
@@ -2,7 +2,11 @@ from django.conf.urls import url
 
 urlpatterns = [
     #social urls
+    url(r'^login$', '', name='login'),
+    url(r'^register$', '', name='register'),
     url(r'^$', 'clouddj.social_views.home', name='home'),
+    url(r'^search$', '', name='search'),
+    url(r'^stream$', '', name='stream'),
 
     #music urls
     url(r'^upload$', 'clouddj.music_views.upload', name='upload')
