81e7d84a595b16dedab55c045def660b1287394d
aalique <aalique.grahame@gmail.com>
2014-11-19 14:56:55 -0500

    likes with js ALMOST working

--- cdj_proj/clouddj/static/js/like.js
+++ cdj_proj/clouddj/static/js/like.js
@@ -17,14 +17,14 @@ $(".like-btn").click(function( event ) {
         success: function( data ) {
             var post_id = data['post_id'];
             var like_button = $("#"+post_id).prev().find(".like-btn");
-            var count = parseInt(form.parent().next().find(".like-count").html());
+            var count = parseInt($("#"+post_id).prev().find(".like-count").html());
             if (data['liked'] == "True") {
                 like_button.addClass("lit-like");
-                form.parent().next().find(".like-count").html((count + 1).toString());
+                $("#"+post_id).prev().find(".like-count").html((count + 1).toString());
             }
             if (data['unliked'] == "True") {
                 like_button.removeClass("lit-like");
-                form.parent().next().find(".like-count").html((count - 1).toString());
+                $("#"+post_id).prev().find(".like-count").html((count - 1).toString());
             }
         }
     });
