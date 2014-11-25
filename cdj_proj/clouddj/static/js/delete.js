fc4bbdd89e3466fc6607342a9602e96dd23c964d
aalique <aalique.grahame@gmail.com>
2014-11-25 00:00:21 -0500

    fixed deleting posts

--- cdj_proj/clouddj/static/js/delete.js
+++ cdj_proj/clouddj/static/js/delete.js
@@ -10,6 +10,8 @@ $(".del-btn").click(function( event ) {
 
             var post_id = data['post_id'];
             $("#"+post_id).parent().parent().next().remove();
+            $("#"+post_id).parent().parent().prev().prev().remove();
+            $("#"+post_id).parent().parent().prev().remove();
             $("#"+post_id).parent().parent().remove();
         }
     });
