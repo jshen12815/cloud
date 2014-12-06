dd9149635435392180167f4a88e87325c8d23ee9
Jason Hugh Sam <jhughsam@gmail.com>
2014-12-06 06:04:58 -0500

    updated rate to include competition posts

--- clouddj/social_views.py
+++ clouddj/social_views.py
@@ -71,45 +71,59 @@ def rate(request,id):
     data = {}
     data['post_id'] = id
     rating=request.POST['rateval']
-   
+
+    # get the competition for the post if it exists
+    try:
+        competition = post.comp.all()[:1].get()
+        time = datetime.datetime
+        # if the user (isn't judge or creator) or (competition is over)
+        # don't let them rate it!
+        if ((request.user.profile not in competition.judges.all()) and \
+            request.user.profile != competition.creator) or \
+            time >= competition.end:
+            return redirect(request.META.get('HTTP_REFERER'))
+    except ObjectDoesNotExist:
+        pass
+
     try: 
         userrating = Rating.objects.get(profile = request.user.profile, post = post)
         print "i already rated"
-        if id:
-            numratings=int(post.numratings)
-            olduserrating = userrating.rating
-            overallrating = float(post.overallrating)
 
-            new_ratings = (numratings * overallrating) 
-            new_ratingsa = new_ratings + int(rating) - int(olduserrating)
-            new_rating = new_ratingsa/numratings
+        numratings=int(post.numratings)
+        olduserrating = userrating.rating
+        overallrating = float(post.overallrating)
 
-            post.overallrating = new_rating
-            post.showrating = int(post.overallrating)
-            post.save()
+        new_ratings = (numratings * overallrating) 
+        new_ratingsa = new_ratings + int(rating) - int(olduserrating)
+        new_rating = new_ratingsa/numratings
 
-            userrating.rating = rating
-            userrating.save()
+        post.overallrating = new_rating
+        post.showrating = int(post.overallrating)
+        post.save()
+
+        userrating.rating = rating
+        userrating.save()
 
  
     except ObjectDoesNotExist:
         print "newrating"
+
         newrating = Rating(profile=request.user.profile, rating=rating, post=post)
         newrating.save()
-        if id:
-            numratings=int(post.numratings)
-            if (post.overallrating == None):
-                overallrating = 0.0
-            else:
-                overallrating = float(post.overallrating)
-            new_ratings = (numratings * overallrating) 
-            new_ratingsa = new_ratings+ int(rating)
-            new_num_ratings = numratings + 1
-            new_rating = new_ratingsa/new_num_ratings
-            post.overallrating = new_rating
-            post.numratings = new_num_ratings
-            post.showrating = int(post.overallrating)
-            post.save()
+
+        numratings=int(post.numratings)
+        if (post.overallrating == None):
+            overallrating = 0.0
+        else:
+            overallrating = float(post.overallrating)
+        new_ratings = (numratings * overallrating) 
+        new_ratingsa = new_ratings+ int(rating)
+        new_num_ratings = numratings + 1
+        new_rating = new_ratingsa/new_num_ratings
+        post.overallrating = new_rating
+        post.numratings = new_num_ratings
+        post.showrating = int(post.overallrating)
+        post.save()
 
     return redirect(request.META.get('HTTP_REFERER'))
 
@@ -217,7 +231,6 @@ def stream(request):
         context['projects'] = projects
 
     return render(request, 'stream.html', context)
-
 
 @login_required
 def explore(request):
